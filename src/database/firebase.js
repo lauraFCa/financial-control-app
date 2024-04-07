dbdff9b62c7a3280fee015dd2a2f2f0793a3e40c
Laura F Ca <laurafariacastro@gmail.com>
2024-04-06 23:43:04 -0300

    remove cred

--- src/database/firebase.js
+++ src/database/firebase.js
@@ -12,12 +12,7 @@ import { getFirestore } from 'firebase/firestore';
  * Um objeto que contem as credenciais e configuracoes do projeto Firebase
  */
 const firebaseConfig = {
-    "apiKey": "AIzaSyA0aJZfor80yZ6fkWxZlMnC_XrDI5HUBQA",
-    "authDomain": "devmovem-final.firebaseapp.com",
-    "projectId": "devmovem-final",
-    "storageBucket": "devmovem-final.appspot.com",
-    "messagingSenderId": "781338308898",
-    "appId": "1:781338308898:web:4fdbdb8ca7a5d1f840757b"
+    
 };
 
 /**
