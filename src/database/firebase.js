706cb11f4b9f86cfa125a5b9bb14b43c93f1554b
Laura F Ca <laurafariacastro@gmail.com>
2024-04-06 23:47:40 -0300

    Fix credential problem

--- src/database/firebase.js
+++ src/database/firebase.js
@@ -1,19 +1,20 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
+const fs = require('fs');
 
 
 /**
  * O arquivo fornece a configuracao e a instancia do Firebase para serem utilizadas em um aplicativo React Native 
  * A configuracao inclui as credenciais da aplicacao Firebase e a inicializacao do aplicativo e do Firestore
- */
+*/
 
 
 /**
  * Um objeto que contem as credenciais e configuracoes do projeto Firebase
- */
-const firebaseConfig = {
-    
-};
+*/
+const firebaseConfigFile = fs.readFileSync("./firebaseConfig.json", 'utf8');
+const firebaseConfig = JSON.parse(firebaseConfigFile);
+
 
 /**
  *  A instancia inicializada do aplicativo Firebase
